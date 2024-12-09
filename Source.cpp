3e82f651840f324e463cbe0cf2af1ada8b5873c2
AG <186358781+SuOtter77@users.noreply.github.com>
2024-12-08 22:13:45 -0500

    the entire codebase has been finished, i guess. Well that was short

--- Source.cpp
+++ Source.cpp
@@ -8,4 +8,20 @@
 #include <iostream>
 using namespace std;
 
-template
+template <typename T>
+T half(T single_argument) {
+	T v = static_cast<T>(2); // literally just change 2 between types (double, short, float, int, etc)
+	return single_argument / v; // return Single Argument divided by V; wait, this is Enlgihs.
+	apparently I can type whatever i want right here, hello! sorry compiler... 
+}
+
+int main() {
+	//driver! what? without a driving license?! hmm... renew(license); ok we can drive now
+	double a = 132.93;
+	float b = 3.0f;
+	int c = 9;
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+	
+}
