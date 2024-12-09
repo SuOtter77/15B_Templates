623a875bd93412639f0d67745f51e5437bd26612
AG <186358781+SuOtter77@users.noreply.github.com>
2024-12-08 22:01:26 -0500

    Rolling in 3... 2.... (welcome to the show ) TOP OF THE HOUR and we're discussing a certain HAZ BIN who HAS BEEN spotted going around town (welcome to the show!) after a seven year absence! (Welcome to the showww)
    while he hid in radio, we pivoted to video!
    anyways, this is my initial commit

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,11 @@
 // Augustus Gullett
 // CIS 1202.501
 // 12/8/2024
+// I was starting to like this chapter and then C++ got upset
+// because throw "wow" hands off a c-string ...
+
+// This time all the functions will be here. A header for one function is silly.
+#include <iostream>
+using namespace std;
+
+template
