b72d18632915ca6cc5729b4b2a445e7ca4a57bb4
AG <186358781+SuOtter77@users.noreply.github.com>
2024-12-08 22:48:18 -0500

    unelegant solution to, ugh, rounding specifically Ints in a templated function. Coding is derived from mathematics, so elegance is a must.

--- Source.cpp
+++ Source.cpp
@@ -11,18 +11,18 @@ using namespace std;
 template <typename T>
 T half(T single_argument) {
 	T v = static_cast<T>(2); // literally just change 2 between types (double, short, float, int, etc)
-	return single_argument / v; // return Single Argument divided by V; wait, this is Enlgihs.
-	//apparently I can type whatever i want right here, hello! sorry compiler... 
-		// nevermind HAHA
+	if (is_same<T, int>::value) // unelegant solution to rounding ints (ugh)
+		return round(single_argument / 2.0f); // prevent type coercing at first
+	else // this is not ok.
+		return single_argument / v; // return Single Argument divided by V; wait, this is Enlgihs.
 }
 
 int main() {
 	//driver! what? without a driving license?! hmm... renew(license); ok we can drive now
 	double a = 132.93;
 	float b = 3.0f;
-	int c = 12;
+	int c = 7;
 	cout << half(a) << endl;
 	cout << half(b) << endl;
 	cout << half(c) << endl;
-	// btw, you can't round with a template function. it would end up returning an int for a double. You get the point. The type coercion from V is enough, even if it cuts it down to 1 instead of 2 from 1.75. That's ok, we're not rocket scientists here.
 }
