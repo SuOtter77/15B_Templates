40f4ce363c18384ca409f666010920da106dd9e0
AG <186358781+SuOtter77@users.noreply.github.com>
2024-12-08 22:19:48 -0500

    Save compiler from line 15

--- Source.cpp
+++ Source.cpp
@@ -12,14 +12,15 @@ template <typename T>
 T half(T single_argument) {
 	T v = static_cast<T>(2); // literally just change 2 between types (double, short, float, int, etc)
 	return single_argument / v; // return Single Argument divided by V; wait, this is Enlgihs.
-	apparently I can type whatever i want right here, hello! sorry compiler... 
+	//apparently I can type whatever i want right here, hello! sorry compiler... 
+		// nevermind HAHA
 }
 
 int main() {
 	//driver! what? without a driving license?! hmm... renew(license); ok we can drive now
 	double a = 132.93;
 	float b = 3.0f;
-	int c = 9;
+	int c = 12;
 	cout << half(a) << endl;
 	cout << half(b) << endl;
 	cout << half(c) << endl;
